2735b721e02956f6fde1c31a6cbdd75d1fc1dc59
KadirErturk4r <kerturk@4refuel.com>
2022-01-21 14:58:04 -0800

    27x27x27

--- python_codes/draw_neuron.py
+++ python_codes/draw_neuron.py
@@ -19,16 +19,29 @@ ts = 0
 
 def get_data():
     sql = "SELECT nr_x, nr_y, nr_z, " \
-          "case nr_type when 'hidden' then '*' when 'input' then 'o' when 'output' then 'v' end as nr_type, " \
-          "case nr_type when 'hidden' then 'b' when 'input' then 'r' when 'output' then 'g' end as nr_type " \
+          "case nr_type when 'hidden' then '*' when 'input' then 'o' when 'output' then 'v' end as nr_type1, " \
+          "case nr_type when 'hidden' then 'b' when 'input' then 'g' when 'output' then 'r' end as nr_type2 " \
           "FROM neuron WHERE nr_status = 1"
     myCursor.execute(sql)
     t_data = np.array(myCursor.fetchall())
-    x1 = t_data[:, 0].astype('float64')
-    y1 = t_data[:, 1].astype('float64')
-    z1 = t_data[:, 2].astype('float64')
-    a1 = t_data[:, 3].tolist()
-    c1 = t_data[:, 4].tolist()
+    if myCursor.rowcount > 0:
+        x1 = t_data[:, 0].astype('int64')
+        y1 = t_data[:, 1].astype('int64')
+        z1 = t_data[:, 2].astype('int64')
+        a1 = t_data[:, 3].tolist()
+        c1 = t_data[:, 4].tolist()
+    else:
+        x1 = []
+        y1 = []
+        z1 = []
+        a1 = []
+        c1 = []
+        x1.append(0)
+        y1.append(0)
+        z1.append(0)
+        a1.append('*')
+        c1.append('r')
+
     return x1, y1, z1, a1, c1
 
 
@@ -37,24 +50,22 @@ def forward():
     myCursor.callproc('forward', args)
 
 
-def update_graph(ts=None):
-    ts = ts +1
-    x, y, z, a, c = get_data()
-    graph._offsets3d = (x, y, z)
-    plt.title("Time step : " + str(ts))
+def update_graph(num=None):
+    x2, y2, z2, a2, c2 = get_data()
+    graph._offsets3d = (x2, y2, z2)
+    plt.title('Time Step , time={}'.format(num))
     forward()
 
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.axes.set_xlim3d(left=0, right=11)
-ax.axes.set_ylim3d(bottom=0, top=11)
-ax.axes.set_zlim3d(bottom=0, top=11)
+ax.axes.set_xlim3d(left=0, right=27)
+ax.axes.set_ylim3d(bottom=0, top=27)
+ax.axes.set_zlim3d(bottom=0, top=27)
 x, y, z, a, c = get_data()
+
 graph = ax.scatter(x, y, z)
-time.sleep(7)
-ani = matplotlib.animation.FuncAnimation(fig, update_graph, 20, interval=600, blit=False)
+
+anim = matplotlib.animation.FuncAnimation(fig, update_graph, 32, interval=750, blit=False)
 plt.show()
-ani.save("progress.mp4")
-
-
+anim.save("../progress.mp4")
