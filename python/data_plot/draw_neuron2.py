69709e5e7e0d14786f46a702f990b65cc4c19568
KadirErturk4r <kerturk@4refuel.com>
2022-01-24 18:09:29 -0800

    plot change

--- python/data_plot/draw_neuron2.py
+++ python/data_plot/draw_neuron2.py
@@ -1,8 +1,13 @@
-import matplotlib
+import plotly.express as px
 import mysql.connector
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as ani
+import pandas as pd
+import plotly.io as pio
+
+png_renderer = pio.renderers["browser"]
+
+
+# https://plotly.com/python/3d-scatter-plots/
 
 myDB = mysql.connector.connect(
     host="192.168.1.166",
@@ -18,7 +23,7 @@ ts = 0
 def get_data():
     sql = "SELECT nr_x, nr_y, nr_z, " \
           "case nr_type when 'hidden' then '*' when 'input' then 'o' when 'output' then 'v' end as nr_type1, " \
-          "sqrt(nr_outr * nr_outr + nr_outi * nr_outi) as nr_type2 " \
+          "case nr_type when 'hidden' then 'r' when 'input' then 'g' when 'output' then 'b' end as nr_type2 " \
           "FROM neuron WHERE nr_status = 1"
     myCursor.execute(sql)
     t_data = np.array(myCursor.fetchall())
@@ -28,7 +33,7 @@ def get_data():
         y1 = t_data[:, 1].astype('int64')
         z1 = t_data[:, 2].astype('int64')
         a1 = t_data[:, 3].tolist()
-        c1 = t_data[:, 4].astype('float64')
+        c1 = t_data[:, 4].tolist()
     else:
         x1 = []
         y1 = []
@@ -49,32 +54,16 @@ def forward():
     myCursor.callproc('forward', args)
 
 
-def update_graph(num=None):
-    x2, y2, z2, a2, c2, n_rec = get_data()
-    graph._offsets3d = (x2, y2, z2)
-    plt.title('Time Step , ' + str(num) + " n rec " + str(n_rec))
-    forward()
 
+# df = px.data.iris()
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.axes.set_xlim3d(left=0, right=39)
-ax.axes.set_ylim3d(bottom=0, top=39)
-ax.axes.set_zlim3d(bottom=0, top=39)
+x, y, z, a, c, n_recs = get_data()
+df = pd.DataFrame()
+df['x_axis'] = pd.Series(x)
+df['y_axis'] = pd.Series(y)
+df['z_axis'] = pd.Series(z)
+df['color'] = pd.Series(c)
 
-x, y, z, a, c, n_rec = get_data()
-graph = ax.scatter(x, y, z, c=c)
-# ax.view_init(30, 0)
-for i in range(0, 360):
-
-    plt.cla()
-    plt.title('Time Step , ' + str(i) + " n rec " + str(n_rec))
-    forward()
-    x, y, z, a, c, n_rec = get_data()
-    ax.scatter(x, y, z, c=c)
-    ax.set_zlim(0, 40)
-    ax.set_ylim(0, 40)
-    ax.set_xlim(0, 40)
-    plt.draw()
-    plt.pause(1)
+fig = px.scatter_3d(df, x='x_axis', y='y_axis', z='z_axis', color='color')
+fig.show(renderer="png", width=800, height=300)
 
